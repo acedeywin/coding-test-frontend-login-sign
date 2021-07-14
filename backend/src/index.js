56e8af12ea339843794d9c8927f6afe3358743ee
Stephen Okpalaononuju <stephenokpala@gmail.com>
2021-07-14 18:09:31 +0100

    ft-backend task

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import express from "express"
 import expressValidator from "express-validator"
+import cors from "cors"
+import etherUtil from "ethereumjs-util"
 import dotenv from "dotenv-safe"
 
 import { errorResponse, successResponse } from "./utils"
@@ -12,6 +14,7 @@ const port = 3000
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(expressValidator())
+app.use(cors())
 
 app.get("/token", (req, res) => {
   let nonce = Math.floor(Math.random() * 1000000).toString() // in a real life scenario we would random this after each login and fetch it from the db as well
@@ -22,13 +25,25 @@ app.post("/auth", (req, res) => {
 
   // TODO: Validate signature by using eth tools (tip: ethereumjs-util and eth-sig-util)
 
-  const recoveredAddress = "the_address_recovered_from_the_signature"
+  try {
+    const signedMessage = Buffer.from(
+      `\x19Ethereum Signed Message:\n${nonce.toString().length}${nonce}`
+    )
+    const hashedMessage = etherUtil.keccak(signedMessage)
+    const { v, r, s } = etherUtil.fromRpcSig(signature)
+    const prefixedMessage = etherUtil.toBuffer(hashedMessage)
+    const publicKey = etherUtil.ecrecover(prefixedMessage, v, r, s)
+    const publicKeyAddress = etherUtil.pubToAddress(publicKey)
+    const recoveredAddress = etherUtil.bufferToHex(publicKeyAddress)
 
-  if (recoveredAddress !== address) {
-    return errorResponse(res, 401)
+    if (recoveredAddress !== address) {
+      return errorResponse(res, 401)
+    }
+
+    return successResponse(res, "Hello world")
+  } catch (err) {
+    throw err.message
   }
-
-  return successResponse(res, "Hello World!")
 })
 
 app.listen(port, () => {
